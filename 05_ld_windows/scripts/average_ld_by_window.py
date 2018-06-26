474a4feeabc18a3260b007afe79173018866f48c
djcotter <dcotter1@asu.edu>
2018-06-25 17:05:50 -0700

    added modified scripts for LD analysis

---
+++ 05_ld_windows/scripts/average_ld_by_window.py
@@ -0,0 +1,75 @@
+"""
+average_ld_by_window.py
+Daniel Cotter
+Updated: 3/30/17
+
+The purpose of this script is to take pairwise LD output from plink
+and calculate average LD in set windows
+-------------------------------------------------------------------------------
+"""
+
+# import required packages ----------------------------------------------------
+import sys
+import csv
+import argparse
+import ld_analysis
+
+# Set argument variables ------------------------------------------------------
+""" input file: pairwise LD file generated in space-delimited table format
+    by plink using the command 'with-freqs'
+                (essential so the columns are correctly indexed)
+    windows: bed file with desired windows for the analysis
+    ld_bin_size: length in either direction from the focal position in which
+                 we should consider LD (in kilobases)
+    output_file: path to an output file
+"""
+# Parse arguments
+parser = argparse.ArgumentParser(description="Filter window anlysis output.")
+
+parser.add_argument("--plink_ld", required=True,
+                    help="Path to the input plink LD file. This file should" +
+                    "be generated using ")
+parser.add_argument("--windows", required=True,
+                    help="Path to the input window file.")
+parser.add_argument("--output", nargs="?", default=True,
+                    help="Path to output file. If no output file is" +
+                    " provided, default is standard out.")
+parser.add_argument("--binSize", nargs='?', type=int, required=True,
+                    help="Size of the bin that LD is calculated in. This " +
+                    "extends from the current site being analyzed in each " +
+                    "direction. This is in kilobases.")
+
+# check that commands are there
+if len(sys.argv) == 1:
+    parser.print_usage()
+    sys.exit()
+
+# add arguments to args variable
+args = parser.parse_args()
+
+# Script ----------------------------------------------------------------------
+
+# Read in windows file and set starting conditions
+# Open the window file as bed coordinates so that every
+# window_coordinates[i][1] corresponds to the start position
+# and [2] to the end position.
+with open(args.windows, 'rU') as f:
+    window_file = list(csv.reader(f, delimiter='\t'))
+    for window in window_file:
+        window[1] = float(window[1])
+        window[2] = float(window[2])
+
+# Open the LD file and perform the analysis line by line
+ld_calculations = ld_analysis.LD_loop(
+    args.plink_ld, window_file, args.binSize)
+
+# write the results to output_file or standard out depending on args
+if args.output is True:
+    writer = csv.writer(sys.stdout, delimiter='\t')
+    for row in ld_calculations:
+        writer.writerow(row)
+else:
+    with open(args.output, 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter='\t')
+        for row in ld_calculations:
+            writer.writerow(row)
---
+++ 05_ld_windows/scripts/ld_analysis.pyx
@@ -0,0 +1,180 @@
+"""
+ld_analysis.pyx
+Daniel Cotter
+
+Uses cython to speed up the required functions for the LD bin analysis.
+"""
+
+# import required modules -----------------------------------------------------
+import numpy as np
+
+
+# Functions for compiling summary stats ---------------------------------------
+def mean_LD(window_LD_dictionary):
+    """
+    Calculate mean LD in a window by taking all focal positions and their
+    associated distance and R2 values.
+    The structure of the data is {fp: [[distance, R2], [distance, R2] ...]}
+    """
+    values = []
+    for focal_position in window_LD_dictionary:
+        for pair in window_LD_dictionary[focal_position]:
+            values.append(pair[1])
+    mean_LD = np.mean(values)
+    return mean_LD
+
+
+def median_LD(window_LD_dictionary):
+    """
+    Calculate median LD in a window by taking all focal positions and their
+    associated distance and R2 values.
+    The structure of the data is {fp: [[distance, R2], [distance, R2] ...]}
+    """
+    values = []
+    for focal_position in window_LD_dictionary:
+        for pair in window_LD_dictionary[focal_position]:
+            values.append(pair[1])
+    median_LD = np.median(values)
+    return median_LD
+
+
+def LD_percentiles(window_LD_dictionary, lower, upper):
+    """
+    Calculate percintiles of LD values in a window by taking all focal
+    positions and their associated distance and R2 values.
+    The structure of the data is {fp: [[distance, R2], [distance, R2] ...]}
+    """
+    values = []
+    for focal_position in window_LD_dictionary:
+        for pair in window_LD_dictionary[focal_position]:
+            values.append(pair[1])
+    quartiles = [np.percentile(values, lower), np.percentile(values, upper)]
+    return quartiles
+
+
+def R2_values_to_array(window_LD_dictionary):
+    """
+    Function to take the R2_Values dictionary for a specific window and
+    return a numpy array of all R2 values within the window
+    """
+    values = []
+    for fp in window_LD_dictionary:
+        for pair in window_LD_dictionary[fp]:
+            values.append(pair[1])
+    return np.asarray(values)
+
+
+def rand_samples(data_array):
+    """
+    Create a randomly dsitributed set of data based on a given array and
+    the length of the given data array
+    """
+    array_len = len(data_array)
+    indices = np.random.random_integers(0, array_len - 1, array_len)
+    return np.take(data_array, indices)
+
+
+def bootstrap_CI_mean(data_array, replicates):
+    """
+    Bootstraps the data to get a 95% confidence interval for the
+    """
+    resamples = []
+    for i in range(replicates):
+        resamples.append(np.mean(rand_samples(data_array)))
+
+    return [np.nanpercentile(resamples, 2.5),
+            np.nanpercentile(resamples, 97.5)]
+
+
+def decay_value(window_LD_dictionary):
+    pass
+
+
+# Loop for passing through the LD summary file --------------------------------
+def LD_loop(input_file, window_coordinates, int ld_bin_size):
+    cdef int win_num, fp, ld_bin, row_1, row_5
+    cdef float row_8
+
+    win_num = 0
+    windows = window_coordinates[win_num]
+    R2_values = {}
+    # The information that is stored as pairwise is not duplicated.
+    # We have to create a matrix that contains values for BP_B > window[i][2]
+    # but BP_B - BP_A < ld_bin
+    reverse_R2 = {}
+    ld_bin = ld_bin_size * 1000
+    fp = 0  # initalize the focal position as 0
+    results = []  # initialize an empty results list
+    with open(input_file, 'rU') as f:
+        for line in f:
+            row = line.split()
+
+            # skip the header line of the file
+            if row[0] == 'CHR_A':
+                continue
+
+            # Declare all relevant values
+            a1 = int(row[1])  # BP_A
+            a5 = int(row[5])  # BP_B
+            a8 = float(row[8])  # R2
+            row_1 = a1
+            row_5 = a5
+            row_8 = a8
+
+            # initializes the focal position for the first line of the file
+            if fp == 0:
+                fp = row_1
+                R2_values[fp] = []
+
+            # change script paramaters and initialize a new list when reaching
+            # a new focal position
+            if row_1 != fp:
+                fp = row_1
+
+                # if the focal position lies outside of the current window,
+                # change the window index, calculate a summary statistic,
+                # and clear R2_values from memory
+                if (fp > windows[2]):
+                    bootstrap = bootstrap_CI_mean(
+                        R2_values_to_array(R2_values), 1000)
+                    results.append([windows[1], windows[2],
+                                    mean_LD(R2_values), bootstrap[0],
+                                    bootstrap[1]])
+                    print("{} base pairs analyzed across {}".format(
+                        windows[1], windows[0]))
+                    R2_values = {}
+                    win_num += 1
+                    windows = window_coordinates[win_num]
+
+                R2_values[fp] = []
+
+            # if the site being compared is within ld_bin of the focal
+            # position, store the distance and R2 value in the dictionary
+            # as a list
+            if (abs(row_5 - row_1) < ld_bin):
+                R2_values[fp].append([row_5 - row_1, row_8])
+
+            # if the site in position 2 lies outside the current window,
+            # but (BP_B - BP_A) lies within ld_bin, we need to save the
+            # information for analyses of future windows
+            if (row_5 > windows[2]) and (abs(row_5 - row_1) < ld_bin):
+                if row_5 in reverse_R2:
+                    reverse_R2[row_5].append([row_5 - row_1, row_8])
+                else:
+                    reverse_R2[row_5] = [[row_5 - row_1, row_8]]
+
+            # check the reverse list to see if there are any saved data for
+            # the current focal position, then combine the two lists
+            if fp in reverse_R2:
+                R2_values[fp] = R2_values[fp] + reverse_R2[fp]
+                del reverse_R2[fp]
+
+    # once the last line has been reached, R2_values will have all the
+    # information correspoding to the last window
+    # add the summary of these values to the results file
+    bootstrap = bootstrap_CI_mean(R2_values_to_array(R2_values), 1000)
+    results.append([windows[1], windows[2], mean_LD(R2_values),
+                    bootstrap[0], bootstrap[1]])
+
+    # finally, return the results file
+    return results
