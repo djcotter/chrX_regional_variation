07cb6e81cbd9e36235c4089318ec57d8c596b831
djcotter <dcotter1@asu.edu>
2018-08-10 10:36:11 -0700

    fix log typo

--- 02_diversity_by_site/scripts/Diversity_from_VCF_pyvcf_Output_pi_per_site_per_population_ignoreINDELs_Hohenlohe.py
+++ 02_diversity_by_site/scripts/Diversity_from_VCF_pyvcf_Output_pi_per_site_per_population_ignoreINDELs_Hohenlohe.py
@@ -254,12 +254,12 @@ for record in vcf_reader:
             pop[2].append([record.CHROM, record.POS, pi_site(
                 [allele_count[x] for x in allele_count])])
 
-    # display ythe current count and provide time elapsed per 10000 records
+    # display the current count and provide time elapsed per 10000 records
     counter += 1
     if counter % 10000 == 0:
         time_elapsed = time.time() - start_time
         print("{} records complete ".format(counter) +
-              "(1000 records in {} sec)...".format(time_elapsed))
+              "(10,000 records in {} sec)...".format(time_elapsed))
         start_time = time.time()
 
 print("VCF traversal complete")
