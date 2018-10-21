09da0d253e520468e28db9b55fed7a50fa11d07b
djcotter <daniel.j.cotter@gmail.com>
2018-10-20 22:45:26 -0700

    fix io error

--- 06_figures/scripts/plot_LD_byRegion.R
+++ 06_figures/scripts/plot_LD_byRegion.R
@@ -3,8 +3,10 @@ suppressPackageStartupMessages(require(optparse))
 suppressPackageStartupMessages(require(ggpubr))
 
 option_list = list(
-  make_option(c('--divergenceRatios'), type='character', default=NULL,
-              help="path to divergenceRatios file"),
+  make_option(c('--chrX'), type='character', default=NULL,
+              help="path to chrX regions data"),
+  make_option(c('--chr8'), type='character', default=NULL,
+              help="path to chr8 data"),
   make_option(c('-o', '--output'), type='character', default=NULL,
               help="path to output file"),
   make_option(c('--width'), type='double', default=5.0,
@@ -21,7 +23,7 @@ opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
 opt$units = tolower(opt$units)
 
-if(is.null(opt$divergenceRatios) || is.null(opt$output)) {
+if(is.null(opt$chrX) || is.null(opt$chr8) || is.null(opt$output)) {
   print_help(opt_parser)
   stop("Input and Output files must be specified.", call.=FALSE)
 }
@@ -37,8 +39,6 @@ if( !(opt$width > 0 && opt$height > 0) ){
 }
 
 # read in the data
-data <- read.delim(opt$divergenceRatios, header=TRUE)
-
 chrX_data <- read.delim(opt$chrX, header=FALSE)
 chrX_data$regions <- c("PAR1", "nonPAR", "XTR", "PAR2")
 chr8_data <- read.delim(opt$chr8, header=FALSE)
@@ -48,11 +48,11 @@ data <- rbind(chrX_data, chr8_data)
 data$regions <- factor(data$regions, levels=c('PAR1', 'XTR', 'nonPAR', 'PAR2', 'chr8'))
 
 p1 <- ggplot(data, aes(x=regions)) + theme_pubr() +
-  geom_col(aes(y=V3), color='black') +
-  geom_errorbar(aes(ymin=V4, ymax=V5), width=0.75, size=0.8) +
+  geom_col(aes(y=V3), color='black') + 
+  geom_errorbar(aes(ymin=V4, ymax=V5), width=0.75, size=0.8) + 
   labs(x = "Regions", y=expression(bold("Average R"^"2"))) +
   scale_x_discrete(labels=c("PAR1", "XTR", "chrX", "PAR2", "chr8")) +
-  coord_cartesian(ylim=c(0.3, 0.65)) +
+  coord_cartesian(ylim=c(0.3, 0.65)) + 
   theme(axis.title = element_text(size=16, face="bold"),
         axis.text = element_text(size=14))
 
